b317dfa0be1d6a166365c17507db1cd24814ced0
Simeon Yankov <simeon.yankov@elando.com>
2024-04-04 11:24:43 +0300

    Add prefix and refactor code

--- MessageWithTraceId.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ServiceBusTools
-{
-    public class MessageWithTraceId<T> where T : class, new()
-    {
-        public string RequestId { get; set; } = "";
-        public T Model { get; set; } = new T();
-    }
-}
